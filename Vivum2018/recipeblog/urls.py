2fe80191d0cca8760cc32a6b3ee78a7eddcc95b9
FenrisWolf123 <bhargav.kulkarni1@gmail.com>
2018-08-22 19:22:31 +0530

    url namespacing

--- Vivum2018/recipeblog/urls.py
+++ Vivum2018/recipeblog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipeblog'
 urlpatterns = [
 	path('', views.index, name='index'),
 	#path('<recipe>/', views.recipe, name='recipe')
