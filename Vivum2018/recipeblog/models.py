788b3fb804e969737eadf43d6ee53530c6fed974
greatblitz982 <greatblitz982@gmail.com>
2018-08-22 16:26:10 +0530

    Initial setup for Post model

--- Vivum2018/recipeblog/models.py
+++ Vivum2018/recipeblog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import json
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	ingredients = models.CharField(max_length=10000)
+	contents = models.TextField()
+	picture = models.URLField(max_length=500)
+
+	def setIngredients(self, ingredientsText):
+		self.ingredients = json.dumps(ingredientsText)
+
+	def getIngredientsList(self):
+		return json.loads(self.ingredients)
