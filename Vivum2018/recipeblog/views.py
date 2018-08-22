c537619803f8c22ba989c42d4c3d3f810586b19c
greatblitz982 <greatblitz982@gmail.com>
2018-08-22 22:49:19 +0530

    Form push pt1

--- Vivum2018/recipeblog/views.py
+++ Vivum2018/recipeblog/views.py
@@ -4,3 +4,5 @@ from .models import Post
 def index(request):
 	lastRecipes = Post.objects.order_by('-datePosted')
 	return render(request, 'recipeblog/index.html', {'lastRecipes': lastRecipes})
+
+#def recipe(request, recipe):
