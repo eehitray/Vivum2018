3e05559433ab1366bca6b9a6bcc548402447901c
greatblitz982 <greatblitz982@gmail.com>
2018-08-22 23:12:38 +0530

    Revert "Temp fix to run"
    This reverts commit 775a50ccf9fc4b397f27c5f271ca2da8bb899355.

--- Vivum2018/recipeblog/urls.py
+++ Vivum2018/recipeblog/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'recipeblog'
 urlpatterns = [
 	path('', views.index, name='index'),
-	#path('<recipeName>/', views.recipe, name='recipe')
+	path('<recipeName>/', views.recipe, name='recipe')
 ]
