7eefbce79c3236d0b0521c25a1478a5980c6dfab
FenrisWolf123 <bhargav.kulkarni1@gmail.com>
2018-08-22 23:53:37 +0530

    massive reverts

--- Vivum2018/recipeblog/models.py
+++ Vivum2018/recipeblog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 import json
 
+from django.db import models
+import json
+
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	ingredients = models.CharField(max_length=10000)
@@ -13,3 +16,6 @@ class Post(models.Model):
 
 	def getIngredientsList(self):
 		return json.loads(self.ingredients)
+
+	def __str__(self):
+		return self.title
