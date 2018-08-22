be2f7883b4ac4ef3869d2cb7743d292ea0971948
greatblitz982 <greatblitz982@gmail.com>
2018-08-22 23:12:57 +0530

    Revert "Form push pt1"
    This reverts commit c537619803f8c22ba989c42d4c3d3f810586b19c.

--- Vivum2018/recipeblog/views.py
+++ Vivum2018/recipeblog/views.py
@@ -4,5 +4,3 @@ from .models import Post
 def index(request):
 	lastRecipes = Post.objects.order_by('-datePosted')
 	return render(request, 'recipeblog/index.html', {'lastRecipes': lastRecipes})
-
-#def recipe(request, recipe):
