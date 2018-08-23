c4a8795b16fdaf2fcf47a85329637570a44a7d51
greatblitz982 <greatblitz982@gmail.com>
2018-08-24 04:11:54 +0530

    Fix nutrition manager

--- Vivum2018/accounts/models.py
+++ Vivum2018/accounts/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 class FalseUser(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	height = models.FloatField(default='-1')
-	weight = models.FloatField(default='-1')
-	age = models.IntegerField(default='-1')
+	height = models.FloatField(default=-1)
+	weight = models.FloatField(default=-1)
+	age = models.IntegerField(default=-1)
 	gender = models.CharField(max_length=1, default='M')
-	calorieGoal = models.IntegerField(default='-1')
+	calorieGoal = models.IntegerField(default=-1)
 	calorieDate = models.DateField(null=True, blank=True)
 
 	def __str__(self):
--- Vivum2018/nutritiontracker/views.py
+++ Vivum2018/nutritiontracker/views.py
@@ -13,7 +13,7 @@ def renderAll(request):
 		if (falseUser.height == -1):
 			return render(request, 'nutritiontracker/index.html', {'displayForm': 'True'})
 		else:
-			resultsDict = {calGol: falseUser.calorieGoal, calDate: falseUser.calorieDate}
+			resultsDict = {'calGol': falseUser.calorieGoal, 'calDate': falseUser.calorieDate}
 			return render(request, 'nutritiontracker/index.html', {'resultsDict': resultsDict})
 
 def submitFood(request):
@@ -23,10 +23,14 @@ def submitFood(request):
 		endpoint = 'https://api.edamam.com/api/food-database/parser'
 		param = {'ingr': request.POST['food'], 'app_id': 'f9e2673a', 'app_key': '98567a484e370e9f073756bf971463e8'}
 		r = requests.get(url=endpoint, params = param)
+		print(r)
 		j = r.json()
 
-		rData = {name: request.POST['food'], cals: (j['parsed'][0]['food']['nutrients']['ENERC_KCAL'] * int(request.POST['qty']))/100}
-		resultsDict = {calGol: falseUser.calorieGoal, calDate: falseUser.calorieDate, recipeFound: rData}
+		rData = {'name': request.POST['food'], 'cals': (j['parsed'][0]['food']['nutrients']['ENERC_KCAL'] * int(request.POST['qty']))/100}
+		resultsDict = {'calGol': falseUser.calorieGoal, 'calDate': falseUser.calorieDate, 'recipeFound': rData}
+
+		falseUser.calorieGoal -= (j['parsed'][0]['food']['nutrients']['ENERC_KCAL'] * int(request.POST['qty']))/100
+		falseUser.save()
 		return render(request, 'nutritiontracker/index.html', {'resultsDict': resultsDict})
 	else:
 		return render(request, '404.html')
@@ -42,15 +46,16 @@ def updateDetails(request):
 		except:
 			return render(request, 'nutritiontracker/index.html', {error: 'You have left one or more fields blank.'})
 		else:
-			BMR = 10 * weight - 5 * age
+			print(weight, height, age)
+			BMR = 10 * float(weight) - 5 * int(age)
 			if cur_false_user.gender == 'M':
-				BMR += 6.25 * height + 5
+				BMR += 6.25 * float(height) + 5
 			else:
-				BMR += 6.25 * height - 161
+				BMR += 6.25 * float(height) - 161
 
-			cur_false_user.age=age,
-			cur_false_user.height=height,
-			cur_false_user.weight=weight,
+			cur_false_user.age=int(age)
+			cur_false_user.height=float(height)
+			cur_false_user.weight=float(weight)
 			cur_false_user.calorieGoal=BMR*7
 			cur_false_user.calorieDate= datetime.date.today() + datetime.timedelta(days=7)
 
