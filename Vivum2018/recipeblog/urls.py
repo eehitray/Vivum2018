54aa80c4d44a557be743526dd8683f29ea488847
greatblitz982 <greatblitz982@users.noreply.github.com>
2018-08-23 14:02:52 +0530

    Recipe detail push

--- Vivum2018/recipeblog/urls.py
+++ Vivum2018/recipeblog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	path('', views.index, name='index'),	
     path('index.html', TemplateView.as_view(template_name="index.html")),
     path('form.html', TemplateView.as_view(template_name="recipeblog/form.html"), name='form'),
-	path('submit', views.submit, name='submit')
+	path('submit', views.submit, name='submit'),
+	path('<rname>', views.blog, name='blog'),
 ]
