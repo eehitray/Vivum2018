ba5b14e9e3ad6f0a437873d3eb3cb17cfb26e204
greatblitz982 <greatblitz982@gmail.com>
2018-08-23 20:06:40 +0530

    Fix index urls

--- Vivum2018/Vivum2018/urls.py
+++ Vivum2018/Vivum2018/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipe/', include('recipeblog.urls')),
     path('', TemplateView.as_view(template_name="index.html")),
-    path('index.html', TemplateView.as_view(template_name="index.html")),
+    path('index.html', TemplateView.as_view(template_name="index.html"), name='mainIndex'),
     path('accounts/', include('accounts.urls')),
 ]
