e26f0c40ca53019990776a8eebc076576ef9fd4e
greatblitz982 <greatblitz982@gmail.com>
2018-08-24 02:25:12 +0530

    Temp push to nutrition tracker

--- Vivum2018/nutritiontracker/views.py
+++ Vivum2018/nutritiontracker/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def renderAll(request):
+	if not request.user.is_authenticated:
+		return render(request, 'nutritiontracker/index.html', {'error': 'You must be signed in to view this page.'})
+	else:
+		falseUser = FalseUser.objects.get(user=request.user)
+		if (falseUser.height == -1):
+			return render(request, 'nutritiontracker/index.html', {'displayForm': 'True'})
+		else:
+			resultsDict = {calGol: falseUser.calorieGoal, calDate: falseUser.calorieDate}
+			return render(request, 'nutritiontracker/index.html', {'resultsDict': resultsDict})
