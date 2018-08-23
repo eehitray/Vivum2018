82ae744f06dc8377e2627aef89c8f0fb73f154f9
greatblitz982 <greatblitz982@gmail.com>
2018-08-24 03:10:20 +0530

    Final theoretical nutritional tracker

--- Vivum2018/nutritiontracker/views.py
+++ Vivum2018/nutritiontracker/views.py
@@ -15,15 +15,19 @@ def renderAll(request):
 def submitFood(request):
 	if request.POST:
 		falseUser = FalseUser.objects.get(user=request.user)
-		resultsDict = {calGol: falseUser.calorieGoal, calDate: falseUser.calorieDate}
+		
 		endpoint = 'https://api.edamam.com/api/food-database/parser'
 		param = {'ingr': request.POST['food'], 'app_id': 'f9e2673a', 'app_key': '98567a484e370e9f073756bf971463e8'}
 		r = requests.get(url=endpoint, params = param)
 		j = r.json()
+
+		rData = {name = request.POST['food'], cals = (j['parsed'][0]['food']['nutrients']['ENERC_KCAL'] * int(request.POST['qty']))/100}
+		resultsDict = {calGol: falseUser.calorieGoal, calDate: falseUser.calorieDate, recipeFound = rData}
+		return render(request, 'nutritiontracker/index.html', {'resultsDict': resultsDict})
 	else:
 		return render(request, '404.html')
 
-def update_details(request):
+def updateDetails(request):
     cur_false_user = FalseUser.objects.get(user=request.user)
 
     if request.method == 'POST':
