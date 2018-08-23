63627c03465ead7e672ebfb619ecd4560ad5084a
greatblitz982 <greatblitz982@gmail.com>
2018-08-24 02:50:40 +0530

    Temp nutrition tracker

--- Vivum2018/nutritiontracker/urls.py
+++ Vivum2018/nutritiontracker/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'nutritiontracker'
 urlpatterns = [
     path('', views.renderAll, name='renderAll'),
+    path('404.html', TemplateView.as_view(template_name="404.html"), name='error'),
+    path('updatedetails', views.updateDetails, name='updateDetails'),
+    path('submitfood', views.submitFood, name='submitFood'),
 ]
--- Vivum2018/nutritiontracker/views.py
+++ Vivum2018/nutritiontracker/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 def renderAll(request):
 	if not request.user.is_authenticated:
@@ -10,3 +11,14 @@ def renderAll(request):
 		else:
 			resultsDict = {calGol: falseUser.calorieGoal, calDate: falseUser.calorieDate}
 			return render(request, 'nutritiontracker/index.html', {'resultsDict': resultsDict})
+
+def submitFood(request):
+	if request.POST:
+		falseUser = FalseUser.objects.get(user=request.user)
+		resultsDict = {calGol: falseUser.calorieGoal, calDate: falseUser.calorieDate}
+		endpoint = 'https://api.edamam.com/api/food-database/parser'
+		param = {'ingr': request.POST['food'], 'app_id': 'f9e2673a', 'app_key': '98567a484e370e9f073756bf971463e8'}
+		r = requests.get(url=endpoint, params = param)
+		j = r.json()
+	else:
+		return render(request, '404.html')
