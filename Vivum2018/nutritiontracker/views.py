87c54f64842b03b0302c005d6eb5cf04b6c804ca
FenrisWolf123 <bhargav.kulkarni1@gmail.com>
2018-08-24 02:55:21 +0530

    update views

--- Vivum2018/nutritiontracker/views.py
+++ Vivum2018/nutritiontracker/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 import requests
 
@@ -23,3 +22,32 @@ def submitFood(request):
 		j = r.json()
 	else:
 		return render(request, '404.html')
+
+def update_details(request):
+    cur_false_user = FalseUser.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        try:
+            age = request.POST['age']
+            height = request.POST['height']
+            weight = request.POST['weight']
+        except:
+			return render(request, 'nutritiontracker/index.html', {error: 'You have left one or more fields blank.'})
+        else:
+            BMR = 10 * weight - 5 * age
+            if cur_false_user.gender = 'M':
+                BMR += 6.25 * height + 5
+            else:
+                BMR += 6.25 * height - 161
+            f = FalseUser (
+                age=age,
+                height=height,
+                weight=weight,
+                calorieGoal=BMR*7
+            )
+
+            f.save()
+
+            return render()
+    else:
+        return render(request, '404.html')
